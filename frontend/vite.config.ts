49d263e16c8e91934372842fe6c64d034fb0b710
yanzhichao521 <yanzhichao521@users.noreply.github.com>
2026-02-26 14:00:43 +0800

    Fix Rollup optional dependencies issue

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,6 +3,11 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      cache: false
+    }
+  },
   server: {
     host: '0.0.0.0',
     port: 3000,
