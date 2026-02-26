711c276c1ee63cf715059b3bf9154e34f85babc0
yanzhichao521 <yanzhichao521@users.noreply.github.com>
2026-02-26 14:23:28 +0800

    Set environment variable to skip Rollup native bindings

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+process.env.ROLLUP_SKIP_NATIVE_BINDINGS = '1'
+
 export default defineConfig({
   plugins: [react()],
   build: {
